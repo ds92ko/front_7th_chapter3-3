f9b4a93278b1caf2ed861d0198c406c7bec389a0
dasom <ds92ko@gmail.com>
2025-12-10 06:21:12 +0900

    chore: query provider 및 query client 세팅

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,19 +1,23 @@
 import PostsManagerPage from "../pages/posts-manager"
+import { queryClient } from "../shared/api/query-client"
 import { Footer, Header } from "../shared/ui"
+import Providers from "./providers"
 import Router from "./routes"
 import "./styles/index.css"
 
 const App = () => {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow container mx-auto px-4 py-8">
-          <PostsManagerPage />
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <Providers client={queryClient}>
+      <Router>
+        <div className="flex flex-col min-h-screen">
+          <Header />
+          <main className="flex-grow container mx-auto px-4 py-8">
+            <PostsManagerPage />
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </Providers>
   )
 }
 
