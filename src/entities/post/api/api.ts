d291d5bf1f42649a1b38e6c772b21cb09dada43f
dasom <ds92ko@gmail.com>
2025-12-12 02:27:15 +0900

    feat: updatePost, deletePost 추가

--- src/entities/post/api/api.ts
+++ src/entities/post/api/api.ts
@@ -1,7 +1,17 @@
 import { apiClient } from "../../../shared/api/base"
-import { PostsParams, PostsResponse } from "../model/types"
+import { DeletePostResponse, PostsParams, PostsResponse, UpdatePostBody, UpdatePostResponse } from "../model/types"
 
 export const getPosts = async (params: PostsParams) => {
   const response = await apiClient.get<PostsResponse>("/api/posts", params)
+  return response
+}
+
+export const updatePost = async (id: number, body: UpdatePostBody) => {
+  const response = await apiClient.put<UpdatePostResponse>(`/api/posts/${id}`, body)
+  return response
+}
+
+export const deletePost = async (id: number) => {
+  const response = await apiClient.delete<DeletePostResponse>(`/api/posts/${id}`)
   return response
 }
--- src/entities/post/model/types.ts
+++ src/entities/post/model/types.ts
@@ -11,6 +11,18 @@ export interface Post {
   views: number
 }
 
+export interface UpdatePostBody extends Record<string, unknown> {
+  title: string
+  body: string
+}
+
+export type UpdatePostResponse = Omit<Post, "views">
+
+export interface DeletePostResponse extends Post {
+  isDeleted: boolean
+  deletedOn: string
+}
+
 export interface PostsResponse {
   posts: Post[]
   skip: number
